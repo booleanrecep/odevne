cfb52ab19512c3ddc5f6780eba0c37e080f43ecb
Recep Öztürk <turkishteacher23@gmail.com>
2020-10-23 15:03:15 +0300

    7.try

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import favicon from "../images/favicon.png";
+import { assets } from "../images/svg/ecology/index";
 
 const Seo = () => {
   return (
@@ -9,7 +10,29 @@ const Seo = () => {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
       <title>ÖDEV NE?</title>
-      
+      <meta name="description" content="Ödev Takip Uygulaması" />
+      <meta property="og:url" content="https://odevne.netlify.app/#/" />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content="Ödev Takip Uygulaması" />
+      <meta
+        property="og:description"
+        content="Öğrenci ve Öğretmenler İçin Ödev Takibi"
+      />
+      <meta
+        property="og:image"
+        content={assets.op}
+      />
+
+      <meta name="twitter:card" content="summary" />
+      <meta property="twitter:title" content="Ödev Takip Uygulaması" />
+      <meta
+        property="twitter:description"
+        content="Öğrenci ve Öğretmenler İçin Ödev Takibi"
+      />
+      <meta
+        property="twitter:image"
+        content={assets.op}
+      />
       <meta property="twitter:url" content="https://odevne.netlify.app/#/" />
     </Helmet>
   );
